4ca616e4db1930e481103f95b9ab5543db84c4d9
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-04-18 14:24:49 -0500

    Update 4D_Pteranodon.py

--- 4D_Pteranodon.py
+++ 4D_Pteranodon.py
@@ -87,9 +87,7 @@ def main():
 				squawk, squawk_length = get_squawk()
 				
 			if red_button.is_pressed:
-				print("Exiting program.\n")
-				release_gpio_pins()
-				exit()
+				stop_the_program()
 				
 	except KeyboardInterrupt:
 		stop_the_program()
@@ -103,6 +101,8 @@ def file_check():
 	
 	file_missing_flag = 0
 	
+	sounds = ['Pteranodon1.ogg', 'Pteranodon2.ogg', 'Pteranodon3.ogg', 'Pteranodon4.ogg']
+	
 	logging.info("FILE CHECK")
 	# Check to see if dinosaur_facts.txt file exists
 	if os.path.isfile('Files/dinosaur_facts.txt'):
@@ -110,37 +110,21 @@ def file_check():
 	else:
 		detail_log.error("dinosaur_facts.txt file was not found! Make sure that the dinosaur_facts.txt file exists in the Files folder.")
 		file_missing_flag = 1
-	# Check to see if pteranodon1.mp3 file exists
-	if os.path.isfile('Sounds/pteranodon1.mp3'):
-		logging.info("pteranodon1.mp3 file was found!")
-	else:
-		logging.error("pteranodon1.mp3 file was not found! Make sure that the pteranodon1.mp3 file exists in the 'Sounds' folder.")
-		file_missing_flag = 1
-	# Check to see if pteranodon2.mp3 file exists
-	if os.path.isfile('Sounds/pteranodon2.mp3'):
-		logging.info("pteranodon2.mp3 file was found!")
-	else:
-		logging.error("pteranodon2.mp3 file was not found! Make sure that the pteranodon2.mp3 file exists in the 'Sounds' folder.")
-		file_missing_flag = 1
-	# Check to see if pteranodon3.mp3 file exists
-	if os.path.isfile('Sounds/pteranodon3.mp3'):
-		logging.info("pteranodon3.mp3 file was found!")
-	else:
-		logging.error("pteranodon3.mp3 file was not found! Make sure that the pteranodon3.mp3 file exists in the 'Sounds' folder.")
-		file_missing_flag = 1
-	# Check to see if pteranodon4.mp3 file exists
-	if os.path.isfile('Sounds/pteranodon4.mp3'):
-		logging.info("pteranodon4.mp3 file was found!")
-	else:
-		logging.error("pteranodon4.mp3 file was not found! Make sure that the pteranodon4.mp3 file exists in the 'Sounds' folder.")
-		file_missing_flag = 1
+	
+	# Check to see if sound files exists
+	for sound in sounds:
+		if os.path.isfile('Sounds/' + sound):
+			logging.info("{} file was found!".format(sound))
+		else:
+			logging.error("{} file was not found! Make sure that the {} file exists in the 'Sounds' folder.".format(sound, sound))
+			file_missing_flag = 1
 		
 	# If there are no missing files, return to the main function
 	# Otherwise print out message and exit the program
 	if file_missing_flag == 0:  
 		return
 	else:
-		print("\033[1;31;40m\nCould not run the program. Some files are missing. Check the DinoDen_log.txt file in the 'Files' folder for more information.")
+		print("\033[1;31;40m\nCould not run the program. Some files are missing. Check the log in the 'Files' folder for more information.")
 		stop_the_program()
 
 '''
@@ -152,6 +136,8 @@ def permission_check():
 	
 	permission_flag = 0
 	
+	sounds = ['Pteranodon1.ogg', 'Pteranodon2.ogg', 'Pteranodon3.ogg', 'Pteranodon4.ogg']
+	
 	logging.info("PERMISSION CHECK")
 	# Check to see if user has read access to dinosaur_facts.txt
 	if os.access('Files/dinosaur_facts.txt', os.R_OK):
@@ -159,36 +145,19 @@ def permission_check():
 	else:
 		logging.error("User does not have permission to read the dinosaur_facts.txt file.")
 		permission_flag = 1
-	# Check to see if user has read access to pteranodon1.mp3
-	if os.access('Sounds/pteranodon1.mp3', os.R_OK):
-		logging.info("User has permission to read the pteranodon1.mp3 file.")
-	else:
-		logging.error("User does not have permission to read the pteranodon1.mp3 file.")
-		permission_flag = 1
-	# Check to see if user has read access to pteranodon2.mp3
-	if os.access('Sounds/pteranodon2.mp3', os.R_OK):
-		logging.info("User has permission to read the pteranodon2.mp3 file.")
-	else:
-		logging.error("User does not have permission to read the pteranodon2.mp3 file.")
-		permission_flag = 1
-	# Check to see if user has read access to  pteranodon3.mp3
-	if os.access('Sounds/pteranodon3.mp3', os.R_OK):
-		logging.info("User has permission to read the pteranodon3.mp3 file.")
-	else:
-		logging.error("User does not have permission to read the pteranodon3.mp3 file.")
-		permission_flag = 1
-	# Check to see if user has read access to  pteranodon4.mp3
-	if os.access('Sounds/pteranodon4.mp3', os.R_OK):
-		logging.info("User has permission to read the pteranodon4.mp3 file.")
-	else:
-		logging.error("User does not have permission to read the pteranodon4.mp3 file.")
-		permission_flag = 1
 	
+	# Check to see if user has read access to sound files
+	for sound in sounds:
+		if os.access('Sounds/' + sound, os.R_OK):
+			logging.info("User has permission to read the {} file.".format(sound))
+		else:
+			logging.error("User does not have permission to read the {} file.".format(sound))
+			permission_flag = 1
 	
 	if permission_flag == 0:  
 		return
 	else:
-		print("\033[1;31;40m\nCould not run the program. Check the DinoDen_log.txt file in the 'Files' folder for more information.")
+		print("\033[1;31;40m\nCould not run the program. Check the log in the 'Files' folder for more information.")
 		stop_the_program()
 
 '''
@@ -216,7 +185,7 @@ def empty_file_check(file_name):
 	logging.info("EMPTY FILE CHECK")
 	if file_name == []:
 		logging.error("The dinosaur.txt file is empty. The program won't work.")
-		print("\033[1;31;40mErrors were encountered. Check the log in the 'Files' folder for more details.\033[1;31;40m")
+		print("\033[1;31;40mErrors were encountered. Check the log in the 'Files' folder for more information.")
 		stop_the_program()
 	else:
 		logging.info("The dinosaur.txt file is not empty.(This is good. We don't want an empty file.)")
@@ -252,28 +221,11 @@ function.
 '''
 def get_squawk():
 	
-	squawk1 = "Sounds/pteranodon1.mp3"
-	squawk2 = "Sounds/pteranodon2.mp3"
-	squawk3 = "Sounds/pteranodon3.mp3"
-	squawk4 = "Sounds/pteranodon4.mp3"
-
-	squawk1_length = 6.5     # lenth of file in seconds
-	squawk2_length = 6.5     # lenth of file in seconds
-	squawk3_length = 6       # lenth of file in seconds
-	squawk4_length = 6       # lenth of file in seconds
+	# The Key : Value pair is sound file name : length of file in seconds
+	squawks = {'Sounds/Pteranodon1.ogg' : 6.5 , 'Sounds/Pteranodon2.ogg' : 6.5 , 
+			   'Sounds/Pteranodon3.ogg' : 6 , 'Sounds/Pteranodon4.ogg' : 6 }
 	
-	squawks = [squawk1, squawk2, squawk3, squawk4]
-	
-	squawk = random.choice(squawks)   # Selects random sound file
-	
-	if squawk == squawk1:
-		return squawk, squawk1_length
-	elif squawk == squawk2:
-		return squawk, squawk2_length
-	elif squawk == squawk3:
-		return squawk, squawk3_length
-	else:
-		return squawk, squawk4_length
+	return random.choice(list(squawks.items()))
 
 '''
 The activate_pteranodon funciton takes 2 inputs: squawk and squawk_length. 
@@ -286,7 +238,7 @@ def activate_pteranodon(squawk, squawk_length):
 		pteranodon_motor.value = 0.6       # Controls the motor speed
 	except ValueError:
 		logging.error("A bad value was specified for pteranodon_motor. The value should be between 0 and 1.")
-		print("\033[1;31;40mAn error was encountered. Check the detail log for more information.\n")
+		print("\033[1;31;40mAn error was encountered. Check the log in the 'Files' folder for more information.\n")
 		stop_the_program()
 	pygame.mixer.music.load(squawk)        # Loads the sound file
 	pteranodon_motor_enable.on()           # Starts the motor
