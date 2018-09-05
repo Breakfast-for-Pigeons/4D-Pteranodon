1c08ad98ecd8e1804829eaf2a2e8cd145ec01f94
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-05 18:27:11 -0500

    Update 4D_Pteranodon.py

--- 4D_Pteranodon.py
+++ 4D_Pteranodon.py
@@ -10,7 +10,10 @@ This program is also an example of adding color to text displayed to
 the screen.
 
 Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
 '''
+
 ########################################################################
 #                          Import modules                              #
 ########################################################################
@@ -109,11 +112,11 @@ def file_check():
     # Check to see if sound files exists
     for sound in sounds:
         if os.path.isfile('Sounds/' + sound):
-            logging.info("{} file was found!".format(sound))
+            logging.info("%s file was found!", sound)
         else:
-            logging.error("{} file was not found! Make sure ".format(sound) +
-                          "that the {} file exists in the ".format(sound) +
-                          "'Sounds' folder.")
+            logging.error("%s file was not found! Make sure " +
+                          "that the %s file exists in the " +
+                          "'Sounds' folder.", sound, sound)
             file_missing_flag = 1
 
     # If there are no missing files, return to the main function
@@ -154,10 +157,10 @@ def permission_check():
     for sound in sounds:
         if os.access('Sounds/' + sound, os.R_OK):
             logging.info("User has permission to read the " +
-                         "{} file.".format(sound))
+                         "%s file.", sound)
         else:
             logging.error("User does not have permission to read the " +
-                          "{} file.".format(sound))
+                          "%s file.", sound)
             permission_flag = 1
 
     if permission_flag == 0:
@@ -209,8 +212,8 @@ def empty_file_check(list_name):
               "'Files' folder for more information.")
         stop_the_program()
     else:
-        logging.info("The dinosaur.txt file is not empty.(This is good. We " +
-                     "don't want an empty file.)")
+        logging.info("The dinosaur.txt file is not empty.(This is good. "
+                     "We don't want an empty file.)")
 
 
 def print_header():
@@ -226,15 +229,18 @@ def print_header():
     OK.
     '''
 
+    # The r prefix is to let Pylint know that it is a raw string.
+    # It prevents the Pylint message "Anomolous backslash in string:
+    # string constant might be missing an r prefix"
     print("\n\033[1;33;40m")
-    print("===========================================================================")
-    print("   _  _   ____     ____  _                                 _               ")
-    print("  | || | |  _ \   |  _ \| |_ ___ _ __ __ _ _ __   ___   __| | ___  _ __    ")
-    print("  | || |_| | | |  | |_) | __/ _ \ '__/ _` | '_ \ / _ \ / _` |/ _ \| '_ \   ")
-    print("  |__   _| |_| |  |  __/| ||  __/ | | (_| | | | | (_) | (_| | (_) | | | |  ")
-    print("     |_| |____/   |_|    \__\___|_|  \__,_|_| |_|\___/ \__,_|\___/|_| |_|  ")
-    print("                                                                           ")
-    print("===========================================================================")
+    print(r"===========================================================================")
+    print(r"   _  _   ____     ____  _                                 _               ")
+    print(r"  | || | |  _ \   |  _ \| |_ ___ _ __ __ _ _ __   ___   __| | ___  _ __    ")
+    print(r"  | || |_| | | |  | |_) | __/ _ \ '__/ _` | '_ \ / _ \ / _` |/ _ \| '_ \   ")
+    print(r"  |__   _| |_| |  |  __/| ||  __/ | | (_| | | | | (_) | (_| | (_) | | | |  ")
+    print(r"     |_| |____/   |_|    \__\___|_|  \__,_|_| |_|\___/ \__,_|\___/|_| |_|  ")
+    print(r"                                                                           ")
+    print(r"===========================================================================")
     print("\n")
 
 
